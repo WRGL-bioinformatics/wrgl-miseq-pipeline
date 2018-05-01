0cff40025609864fb2bec1c5963a26409f3e7830
Ben <ben@bensanders.co.uk>
2018-05-01 11:20:41 +0000

    Set to get minimum quality and depth params from the .ini file

--- WRGLPipeline/GenerateGenotypingVCFs.cs
+++ WRGLPipeline/GenerateGenotypingVCFs.cs
@@ -304,7 +304,7 @@ namespace WRGLPipeline
                 foreach (VCFRecordWithGenotype record in parseVCFFile.getVCFRecords["SampleID"])
                 {
                     //store variants that pass qc
-                    if (record.FILTER == "PASS" && record.QUAL >= 30 && System.Convert.ToUInt32(record.infoSubFields["DP"]) >= 1000)
+                    if (record.FILTER == "PASS" && record.QUAL >= parameters.getGenotypingQual && System.Convert.ToUInt32(record.infoSubFields["DP"]) >= parameters.getGenotypingDepth)
                     {
                         tempVariant.CHROM = record.CHROM;
                         tempVariant.REF = record.REF;
